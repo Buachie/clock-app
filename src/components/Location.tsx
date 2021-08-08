47402e1bbd898f3fe6c41eeddd541c22088e05af
Aaron Buachie <aaron.buachiea@gmail.com>
2021-08-08 13:09:16 -0400

    Add menu content and connect to api

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import { fetchLocation, Location } from "../API";
 
+//Arrow Icons
+import ArrowUp from "../assets/desktop/icon-arrow-up.svg";
+
 const UserTime = () => {
   const [location, setLocation] = useState<Location>();
   const [time, setTime] = useState<string>();
+  const [menu, setMenu] = useState<boolean>(false);
 
   const getLocation = async () => {
     const newLocation = await fetchLocation();
@@ -22,6 +26,28 @@ const UserTime = () => {
       <div className="time">
         <h1 className="clock">{time}</h1>
         <p className="time-zone">{location?.abbreviation}</p>
+      </div>
+      <button className="more-info" onClick={() => setMenu(!menu)}>
+        <p>{menu ? "Less" : "More"}</p>
+        <img src={ArrowUp} alt="" />
+      </button>
+      <div className="extra-info">
+        <div className="info">
+          <p>CURRENT TIMEZONE</p>
+          <h2>{location?.timezone}</h2>
+        </div>
+        <div className="info">
+          <p>DAY OF THE YEAR</p>
+          <h2>{location?.day_of_year}</h2>
+        </div>
+        <div className="info">
+          <p>DAY OF THE WEEK</p>
+          <h2>{location?.day_of_week}</h2>
+        </div>
+        <div className="info">
+          <p>WEEK NUMBER</p>
+          <h2>{location?.week_number}</h2>
+        </div>
       </div>
     </div>
   );
